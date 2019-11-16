15ac3388114ec783425420e69bbf53a605546838
AmritpalChera <cheraamritpal@gmail.com>
2019-11-15 20:38:28 -0500

    Updated files

--- Wet.c
+++ Wet.c
@@ -1,18 +1,18 @@
 //assumes mop is connected to motorD
-const int WATER_LEVEL = 10; // distance of water from the peak of the mop in cm
-const int WHEEL_RADIUS = 2.75; //radius of the wheels
-const int OVERSHOOT_ERROR = 3; //in terms of nMotorEncoder
 
 void wet()
 {
 
-    const int TIMES_WET = 2; 
+    const int TIMES_WET = 2; //number of times the robot will wet the mop
+    const int WATER_LEVEL = 10;    // distance of water from the peak of the mop in cm
+    const int WHEEL_RADIUS = 4; //radius of the wheels
+
     for (int wet =0; wet < TIMES_WET; wet++)
     {
         nMotorEncoder[motorD] =0;
         motor[motorD] = 20;
         
-        while (((nMotorEncoder[motorD]*WHEEL_RADIUS*PI)/180)- OVERSHOOT_ERROR < WATER_LEVLE)
+        while (((nMotorEncoder[motorD]*WHEEL_RADIUS*PI)/180)- OVERSHOOT_ERROR < WATER_LEVEL)
         {}
         motor[motorD] = 0;
         wait1MSsec(2000);
@@ -25,8 +25,7 @@ void wet()
 
 }
 
-
-void clean (state* cur_state)
+int main()
 {
-    
+    const int OVERSHOOT_ERROR = 3; //in terms of nMotorEncoder
 }
--- clean.c
+++ clean.c
@@ -1,11 +1,31 @@
 
-void clean (state* cur_state, ifstream & fin)
+#include "PC.FileIO.c"
+void clean (state* cur_state)
 {
-    int x_coord =0; y_coord =0;
-
-    while(fin >> x_coord >> y_coord && cur_state -> clean_cycle)
+    TFileHandle fin;
+    bool (fileOkay = openReadPC(fin, "data.txt"));
+    if (!fileOkay)
     {
-        goto(x_coord, y_coord, cur_state);
+        displayString(5, "Error!"); //I would like to use the speaker here
+        wait1Msec(5000);
     }
+    else
+    {
+        int x_coord = 0, y_coord = 0;
+
+        //infinite for loop, only ends when cur_state does not point to clean_cycle
+        for (int cycle = 0; cycle < 1 && cur_state -> clean_cycle; cycle --)
+        {
+            while (readIntPC(fin, x_coord) && cur_state->clean_cycle)
+            {
+                readIntPC(fin, y_coord); // this is here because unknown how mant arguments readIntPC can take
+                //if it can take more than one, this can be placed in while loop
+                goto(x_coord, y_coord, cur_state);
+            }
+        }   
+    }
+
+    closeFilePC(fin);
+    
     
 }
