294276e89b9c7366b488624f1439ceb15c64f362
sodiumbased <ste53539@gmail.com>
2019-11-17 22:22:29 -0500

    some progress

--- goto.c
+++ goto.c
@@ -22,30 +22,27 @@ int max(int a, int b){
 void display_array(int arg[10][10]){
 	for (int i = 0; i < 10; i++){
 		for (int a = 0; a < 10; a++){
-			printf("%d ", arg[i][a]);
+			if (arg[i][a] < 10)
+				printf("%d  ", arg[i][a]);
+			else
+				printf("%d ", arg[i][a]);
 		}
 		printf("\n");
 	}
 }
 
-bool search(int cur_x, int cur_y, int dest_x, int dest_y, bool map[10][10], int paths[10][10], int weight){
-	if (!map[cur_x][cur_y])
-		return 0;
-	bool isTrivial = 1;
-	for (int delta_x = min(cur_x,dest_x); delta_x < max(cur_x,dest_x); delta_x++){
-		if (!map[delta_x][dest_y])
-			isTrivial = 0;
+void display_bool_array(bool arg[10][10]){
+	for (int i = 0; i < 10; i++){
+		for (int a = 0; a < 10; a++){
+			printf("%d  ", arg[i][a]);
+		}
+		printf("\n");
 	}
-	for (int delta_y = min(cur_y,dest_y); delta_y < max(cur_x,dest_y); delta_y++){
-		if (!map[cur_y][delta_y])
-			isTrivial = 0;
-	}
-	if (isTrivial){
-		printf("Trivial solution\n");
-		return 1;
-	}
-	if (paths[cur_x][cur_y] <= weight && paths[cur_x][cur_y] != 0)
-		return 0;
+}
+
+void search(int cur_x, int cur_y, int dest_x, int dest_y, bool map[10][10], int paths[10][10], int weight){
+	if (!map[cur_x][cur_y] || paths[cur_x][cur_y] <= weight && paths[cur_x][cur_y])
+		return;
 	paths[cur_x][cur_y] = weight;
 	weight++;
 	search(cur_x, cur_y+1, dest_x, dest_y, map, paths, weight);
@@ -54,12 +51,53 @@ bool search(int cur_x, int cur_y, int dest_x, int dest_y, bool map[10][10], int 
 	search(cur_x-1, cur_y, dest_x, dest_y, map, paths, weight);
 }
 	
-void go_to(int x, int y, bool map[10][10], state* cur_state){
-	int paths[10][10] = {{0}};
-	search(cur_state->pos[0], cur_state->pos[1], x, y, map, paths, 0);
+int find_dir(int x, int y, int paths[10][10]){
+	int weight = paths[x][y];
+	int dir[4] ={paths[x][y+1], paths[x+1][y], paths[x][y-1], paths[x-1][y]};
+	for (int i = 0; i < 4; i++){
+		if (min(weight, dir[i]) == dir[i])
+			return i;
+	}
+}
 
+void find_nav(int cur_x, int cur_y, int dest_x, int dest_y, int paths[10][10]){
+	int temp_x = dest_x, temp_y = dest_y;
+	int preferred_dir = find_dir(temp_x, temp_y, paths);
+	while (paths[temp_x][temp_y] != 1){
+		// Look at first the preferred direction then all four directions (0-3 protocol) if paths[temp_x][temp_y] is 1 less than the current weight. It should be noted that when the preferred direction changes that marks a turn and should be handled correctly. PS remember to reverse the driving order as it's writing instructions so it can be read then reversed later.
+	}
+}
+
+void go_to(int x, int y, bool map[10][10], state* cur_state){
+	bool isTrivial = 1;
+	for (int delta_x = min(cur_state->pos[0],x); delta_x < max(cur_state->pos[0],x); delta_x++){
+		if (!map[delta_x][cur_state->pos[1]])
+			isTrivial = 0;
+	}
+	for (int delta_y = min(cur_state->pos[1],y); delta_y < max(cur_state->pos[1],y); delta_y++){
+		if (!map[x][delta_y])
+			isTrivial = 0;
+	}
+	if (isTrivial){
+		if (x - cur_state->pos[0] > 0)
+			turn((1-cur_state->dir)*90);
+		else 
+			turn((3-cur_state->dir)*90);
+		drive(abs(x-cur_state->pos[0]));
+		if (y - cur_state->pos[1] > 0)
+			turn(-cur_state->dir*90);
+		else 
+			turn((2-cur_state->dir)*90);
+		drive(abs(y-cur_state->pos[1]));
+	}
+
+	int paths[10][10] = {{0}};
+	search(cur_state->pos[0], cur_state->pos[1], x, y, map, paths, 1);
+	display_bool_array(map);
+	printf("\n");
 	display_array(paths);
 	//motor functions later lol
+	//output the "trivial solution" messages to a file so I can read the first one
 }
 
 
---
+++ helper.c
@@ -0,0 +1,5 @@
+void drive(float distance, state* cur_state){
+    nMotorEncoder[motorA] = 0;
+    motor[motorA] = motor[motorD] = 30;
+    while ()
+}
