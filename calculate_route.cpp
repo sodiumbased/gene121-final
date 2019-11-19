32531cb53748848bdae4716d348dce4b1ae1a81d
veggieman420 <joshdoesstuff4fun@gmail.com>
2019-11-18 21:04:42 -0500

    Reverted calculate_route back to og

--- calculate_route.cpp
+++ calculate_route.cpp
@@ -1,39 +1,22 @@
 #include <iostream>
+#include <cstdlib>
 #include <fstream>
 
 using namespace std;
 
-const int ROW_SIZE = 85, COL_SIZE = 85;
+const int ROW_SIZE = 10, COL_SIZE = 10;
 
-bool map[ROW_SIZE][COL_SIZE];
-
-bool surroundingGood(int row, int col){
-    if(!map[row][col+1]){
-        return false;
-    }
-    if(!map[row][col-1]){
-        return false;
-    }
-    if(!map[row+1][col]){
-        return false;
-    }
-    if(!map[row-1][col]){
-        return false;
-    }
-    if(!map[row+1][col+1]){
-        return false;
-    }
-    if(!map[row-1][col+1]){
-        return false;
-    }
-    if(!map[row+1][col-1]){
-        return false;
-    }
-    if(!map[row-1][col-1]){
-        return false;
-    }
-    return true;
-}
+bool map[10][10] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+        {0, 0, 0, 1, 1, 0, 0, 0, 0, 0},
+        {0, 0, 0, 1, 1, 1, 1, 1, 1, 0},
+        {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+        {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+        {0, 0, 0, 0, 0, 1, 1, 1, 1, 0},
+        {0, 0, 0, 0, 0, 1, 1, 1, 1, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
 void calculate_route(){
     ofstream fout;
@@ -42,13 +25,13 @@ void calculate_route(){
     bool reverse = true;
     int j = 0;
 
-    for (int i = 0; i < COL_SIZE; i += 2) {
+    for (int i = 0; i < COL_SIZE; ++i) {
         while (j < ROW_SIZE){
-            while(map[(1 - 2 * reverse) * j + reverse * (ROW_SIZE- 1)][i] && surroundingGood((1 - 2 * reverse) * j + reverse * (ROW_SIZE- 1), i)){
+            while(map[(1 - 2 * reverse) * j + reverse * (ROW_SIZE- 1)][i]){
                 fout << (1 - 2 * reverse) * j + reverse * (ROW_SIZE - 1) << " " << i << endl;
                 j++;
 
-                while(map[(1 - 2 * reverse) * j + reverse * (ROW_SIZE - 1)][i] && surroundingGood((1 - 2 * reverse) * j + reverse * (ROW_SIZE- 1), i)){
+                while(map[(1 - 2 * reverse) * j + reverse * (ROW_SIZE - 1)][i]){
                     j++;
                 }
 
@@ -61,4 +44,11 @@ void calculate_route(){
     }
 
     fout.close();
+}
+
+int main() {
+
+    calculate_route();
+
+    return EXIT_SUCCESS;
 }
