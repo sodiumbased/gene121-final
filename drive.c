6ff6915c83dc5e2660b1a01fb0310866bca82638
veggieman420 <joshdoesstuff4fun@gmail.com>
2019-11-18 22:46:24 -0500

    Added drive and turn functions

---
+++ drive.c
@@ -0,0 +1,22 @@
+//
+// Created by Josh Cai on 2019-11-18.
+//
+
+void drive(float distance, state* cur_state, bool canInterrupt){
+    float encoderLimit = distance / (4 * M_Pi * 2) * 360;
+
+    nMotorEncoder[motorA] = 0;
+    motor[motorA] = motor[motorD] = 25;
+    while(!(cur_state->interrupt && canInterrupt) && nMotorEncoder[motorA] < encoderLimit){
+        if(getGyroDegrees(S2) < -1){
+            motor[motorD] = 27;
+        }
+        else if(getGyroDegrees(S2) > 1){
+            motor[motorA] = 27;
+        } else {
+            motor[motorA] = motor[motorD] = 25;
+        }
+    }
+
+    motor[motorA] = motor[motorD] = 0;
+}
---
+++ turn.c
@@ -0,0 +1,19 @@
+//
+// Created by Josh Cai on 2019-11-18.
+//
+
+void turn(int angle, state* cur_state, bool canInterrupt){
+
+    if(angle > 0){
+        motor[motorA] = -15;
+        motor[motorD] = 15;
+        while(!(cur_state->interrupt && canInterrupt) && getGyroDegrees(S2) < (getGyroDegrees(S2) + angle){}
+    } else if(angle < 0){
+        motor[motorA] = 15;
+        motor[motorD] = -15;
+        while(!(cur_state->interrupt && canInterrupt) && getGyroDegrees(S2) > (getGyroDegrees(S2) + angle){}
+    }
+
+    motor[motorA] = motor[motorD] = 0;
+
+}
