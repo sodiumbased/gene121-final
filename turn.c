6e71bb9d8e67fc27a7f87573306d49eb6708899d
veggieman420 <joshdoesstuff4fun@gmail.com>
2019-11-20 19:37:11 -0500

    Turn is bettterrrrr

--- turn.c
+++ turn.c
@@ -16,7 +16,7 @@ void turn(int angle, state* cur_state, bool canInterrupt){
 		motor[motorA] = -15;
 		motor[motorD] = 15;
 		while(!(cur_state->interrupt && canInterrupt) && (getGyroDegrees(S2) < (initAngle + angle))){}
-		} else if(angle < 0){
+	} else if(angle < 0){
 		motor[motorA] = 15;
 		motor[motorD] = -15;
 		while(!(cur_state->interrupt && canInterrupt) && (getGyroDegrees(S2) > (initAngle + angle))){}
@@ -24,40 +24,7 @@ void turn(int angle, state* cur_state, bool canInterrupt){
 
 	motor[motorA] = motor[motorD] = 0;
 
-	int deltaAngle;
-	int numTurns = 0;
-
-	if(angle > 0){
-		deltaAngle = getGyroDegrees(S2)- initAngle;
-	}
-	else if(angle < 0){
-		deltaAngle = initAngle - getGyroDegrees(S2);
-	}
-
-	if(deltaAngle % 90 < 85){
-		deltaAngle -= deltaAngle % 90;
-	}
-	else if(deltaAngle % 90 >= 85){
-		deltaAngle += 90 - (deltaAngle % 90);
-	}
-	numTurns = deltaAngle % 90;
-
-	if(angle > 0){
-		for(int i = 0; i < numTurns; i++){
-			cur_state->dir++;
-			if(cur_state->dir == 4){
-				cur_state->dir = 0;
-			}
-		}
-	}
-	else if(angle < 0){
-		for(int i = 0; i < numTurns; i++){
-			cur_state->dir--;
-			if(cur_state->dir == -1){
-				cur_state->dir = 3;
-			}
-		}
-	}
+	cur_state->dir = (cur_state->dir + angle / 90 + 4) % 4;
 }
 task main(){
 	state cur_state;
